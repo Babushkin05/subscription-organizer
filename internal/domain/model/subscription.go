2b3915a07a9a1c1825d500b0748bf0ab248134c6
Vova Babushkin <babushkinvova05@gmail.com>
2025-07-23 00:03:40 +0300

    add parsing annotation

--- internal/domain/model/subscription.go
+++ internal/domain/model/subscription.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Subscription struct {
-	ID          uuid.UUID
-	ServiceName string
-	Price       int
-	UserID      uuid.UUID
-	StartDate   time.Time
-	EndDate     *time.Time
-	IsDeleted   bool
+	ID          uuid.UUID  `db:"id"`
+	ServiceName string     `db:"service_name"`
+	Price       int        `db:"price"`
+	UserID      uuid.UUID  `db:"user_id"`
+	StartDate   time.Time  `db:"start_date"`
+	EndDate     *time.Time `db:"end_date"`
+	IsDeleted   bool       `db:"is_deleted"`
 }
